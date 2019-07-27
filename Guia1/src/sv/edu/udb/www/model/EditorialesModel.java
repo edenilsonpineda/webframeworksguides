f5e73f9b8dc4be573b3e4281a269a149b3953f0f
samuel <mail@mail.com>
2019-07-27 15:29:41 -0600

    changes in model3

--- Guia1/src/sv/edu/udb/www/model/EditorialesModel.java
+++ Guia1/src/sv/edu/udb/www/model/EditorialesModel.java
@@ -30,6 +30,119 @@ public class EditorialesModel extends Conexion {
 			Logger.getLogger(EditorialesModel.class.getName()).log(Level.SEVERE, null, ex);
 			this.desconectar();
 			return null;
-		}//cierre del catch
-	}//cierre del public list
-}//cierre de la clase principal
+		}
+	}
+	
+	
+	
+	public int insertarEditorial(Editorial editorial) throws SQLException{
+		try {
+			int filasAfectadas = 0;
+			String sql = "INSERT INTO editoriales VALUES(?,?,?,?)";
+			this.conectar();
+			st = conexion.prepareStatement(sql);
+			st.setString(1, editorial.getCodigoEditorial());
+			st.setString(2, editorial.getNombreEditorial());
+			st.setString(3, editorial.getContacto());
+			st.setString(4, editorial.getTelefono());
+			filasAfectadas = st.executeUpdate();
+			this.desconectar();
+			return filasAfectadas;
+		}catch(SQLException ex) {
+			Logger.getLogger(EditorialesModel.class.getName()).log(Level.SEVERE, null, ex);
+			this.desconectar();
+			return 0;
+		}
+	}
+	
+	
+	public Editorial obtenerEditorial(String codigo) throws SQLException{
+		try {
+			String sql = "SELECT * FROM editoriales WHERE codigo_editorial=?";
+			this.conectar();
+			st = conexion.prepareStatement(sql);
+			st.setString(1, codigo);
+			rs = st.executeQuery();
+			if(rs.next()) {
+				Editorial editorial = new Editorial();
+				editorial.setCodigoEditorial(rs.getString("codigo_editorial"));
+				editorial.setNombreEditorial(rs.getString("nombre_editorial"));
+				editorial.setContacto(rs.getString("contacto"));
+				editorial.setTelefono(rs.getString("telefono"));
+				
+				this.desconectar();
+				return editorial;
+			}
+			this.desconectar();
+			return null;			
+		}catch(SQLException ex) {
+			Logger.getLogger(EditorialesModel.class.getName()).log(Level.SEVERE, null, ex);
+			this.desconectar();
+			return null;
+		}
+	}
+	
+	
+	public int modificarEditorial(Editorial editorial) throws SQLException{
+		try {
+			int filasAfectadas = 0;
+			String sql = "UPDATE editoriales SET nombre_editorial = ?, contacto = ?, telefono = ?. codigo_editorial = ?";
+			this.conectar();
+			st = conexion.prepareStatement(sql);
+			st.setString(1, editorial.getNombreEditorial());
+			st.setString(2, editorial.getContacto());
+			st.setString(3, editorial.getTelefono());
+			st.setString(4, editorial.getCodigoEditorial());
+			filasAfectadas = st.executeUpdate();
+			this.desconectar();
+			return filasAfectadas;
+		}catch(SQLException ex) {
+			Logger.getLogger(EditorialesModel.class.getName()).log(Level.SEVERE, null, ex);
+			this.desconectar();
+			return 0;
+		}
+	}
+	
+	
+	public int eliminarEditorial(String codigo) throws SQLException{
+		try {
+			int filasAfectadas = 0;
+			String sql = "DELETE FROM editoriales WHERE codigo_editorial = ?";
+			this.conectar();
+			st = conexion.prepareStatement(sql);
+			st.setString(1, codigo);
+			filasAfectadas = st.executeUpdate();
+			this.desconectar();
+			return filasAfectadas;
+		}catch(SQLException ex) {
+			Logger.getLogger(EditorialesModel.class.getName()).log(Level.SEVERE, null, ex);
+			this.desconectar();
+			return 0;
+		}
+	}
+	
+	
+	public int totalEditoriales() throws SQLException{
+		try {
+			int total = 0;
+			String sql = "SELECT COUNT(codigo_editorial) as totaledit FROM editoriales";
+			this.conectar();
+			st = conexion.prepareStatement(sql);
+			rs = st.executeQuery();
+			while(rs.next()) {
+				total = rs.getInt("totaledit");
+			}
+			this.desconectar();
+			return total;
+		}catch(SQLException ex) {
+			Logger.getLogger(AutoresModel.class.getName()).log(Level.SEVERE, null, ex);
+			this.desconectar();
+			return 0;
+		}
+	}
+	
+	
+	
+}
+
+
--- Guia1/src/sv/edu/udb/www/model/GenerosModel.java
+++ Guia1/src/sv/edu/udb/www/model/GenerosModel.java
@@ -1,5 +1,37 @@
 package sv.edu.udb.www.model;
 
-public class GenerosModel {
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import sv.edu.udb.www.beans.Genero;
 
+public class GenerosModel extends Conexion {
+	
+	public List<Genero> listarGeneros() throws SQLException{
+		try {
+			List<Genero> lista = new ArrayList<>();
+			String sql = "CALL sp_listarGeneros()";
+			this.conectar();
+			cs = conexion.prepareCall(sql);
+			rs = cs.executeQuery();
+			while(rs.next()) {
+				Genero genero = new Genero();
+				genero.setIdGenero(rs.getInt("id_genero"));
+				genero.setNombreGenero(rs.getString("nombre_genero"));
+				genero.setDescripcion(rs.getString("descripcion"));
+				lista.add(genero);
+			}
+			this.desconectar();
+			return lista;
+		}catch(SQLException ex) {
+			Logger.getLogger(GenerosModel.class.getName()).log(Level.SEVERE, null, ex);
+			this.desconectar();
+			return null;
+		}
+	}
+	
+	
+	
 }
